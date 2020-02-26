6c3fb217af7a611c9fc82485995c2a357e224260
Alan Reyes <kutt@katrea.net>
2020-02-26 14:13:21 -0600

    Complete overhaul of the package to increase its quality

--- all/better_settings.py
+++ all/better_settings.py
@@ -11,17 +11,17 @@ configuration for each host without interfering between them.
 import platform
 import sublime
 import datetime
+import logging
 
-SCOPE_HOST_OS = "Host/OS"
-SCOPE_HOST = "Host"
-SCOPE_USER = "User"
+from collections import OrderedDict
+
+SCOPE_DEFAULT = "User"
 SCOPE_OS = "OS"
-SCOPE_DEFAULT = "Default"
-
+SCOPE_HOST = "Host"
+SCOPE_HOST_OS = "Host/OS"
 
 ASK_SCOPE_ITEMS = [
-    [SCOPE_DEFAULT, "Default settings"],
-    [SCOPE_USER, "Normal user settings"],
+    [SCOPE_DEFAULT, "Default user settings"],
     [SCOPE_OS, "Settings specific to this machine OS"],
     [SCOPE_HOST, "Settings specific to this machine"],
     [SCOPE_HOST_OS, "Settings specific to this machine on this OS"],
@@ -29,125 +29,59 @@ ASK_SCOPE_ITEMS = [
 
 __DEBUG__ = False
 
+_valid_scopes = set([SCOPE_DEFAULT, SCOPE_OS, SCOPE_HOST, SCOPE_HOST_OS])
+_logger = logging.getLogger(__name__)
+
 
 class _BetterSettings(object):
-    def __init__(self, basepackage, package_name):
-        self.basepackage = basepackage
-        self.package_name = package_name
-
-        self.hostplatform_settings = sublime.load_settings(
-            self.get_hostplatform_settings_filename()
-        )
-
-        self.host_settings = sublime.load_settings(self.get_host_settings_filename())
-
-        self.platform_settings = sublime.load_settings(
-            self.get_platform_settings_filename()
-        )
-
-        self.user_settings = sublime.load_settings(self.get_settings_filename())
+    def __init__(self, default_settings_dir, scoped_settings):
+        self.default_settings_dir = default_settings_dir
+        self.scoped_settings = scoped_settings
 
     def get(self, setting_name, default=None):
-        return self.hostplatform_settings.get(
-            setting_name,
-            self.host_settings.get(
-                setting_name,
-                self.platform_settings.get(
-                    setting_name, self.user_settings.get(setting_name, default)
-                ),
-            ),
-        )
+        for item in self.scoped_settings.values():
+            if item.settings.has(setting_name):
+                return item.settings.get(setting_name)
+
+        return default
+
+    def get_scoped(self, scope, setting_name, default=None):
+        _ensure_valid_scope(scope)
+        return self.scoped_settings[scope].settings.get(setting_name, default)
 
     def set(self, scope, setting_name, setting_value):
-        if scope == SCOPE_HOST_OS:
-            self.hostplatform_settings.set(setting_name, setting_value)
-        elif scope == SCOPE_HOST:
-            self.host_settings.set(setting_name, setting_value)
-        elif scope == SCOPE_USER:
-            self.user_settings.set(setting_name, setting_value)
-        elif scope == SCOPE_OS:
-            self.platform_settings.set(setting_name, setting_value)
-        elif scope == SCOPE_DEFAULT:
-            raise Exception("You should not save the default settings")
-        else:
-            raise Exception("Invalid scope specified: %s" % (scope))
+        _ensure_valid_scope(scope)
+
+        item = self.scoped_settings[scope]
+        item.settings.set(setting_name, setting_value)
 
     def save(self):
-        sublime.save_settings(self, self.get_hostplatform_settings_filename())
-        sublime.save_settings(self, self.get_host_settings_filename())
-        sublime.save_settings(self, self.get_settings_filename())
-        sublime.save_settings(self, self.get_platform_settings_filename())
+        for item in self.scoped_settings.values():
+            sublime.save_settings(item.filename)
 
     def add_on_change(self, event_name, callback):
-        self.hostplatform_settings.add_on_change(event_name, callback)
-        self.user_settings.add_on_change(event_name, callback)
-        self.platform_settings.add_on_change(event_name, callback)
-        self.host_settings.add_on_change(event_name, callback)
+        for item in self.scoped_settings.values():
+            item.settings.add_on_change(event_name, callback)
 
     def clear_on_change(self, event_name):
-        if self.hostplatform_settings is not None:
-            self.hostplatform_settings.clear_on_change(event_name)
-
-        if self.host_settings is not None:
-            self.host_settings.clear_on_change(event_name)
-
-        if self.platform_settings is not None:
-            self.platform_settings.clear_on_change(event_name)
-
-        if self.user_settings is not None:
-            self.user_settings.clear_on_change(event_name)
-
-    def get_hostplatform_settings_filename(self):
-        return self.get_settings_filename(
-            platform.uname()[1].lower() + " on " + sublime.platform().capitalize()
-        )
-
-    def get_host_settings_filename(self):
-        return self.get_settings_filename(platform.uname()[1].lower())
-
-    def get_platform_settings_filename(self):
-        return self.get_settings_filename(sublime.platform().capitalize())
-
-    def get_settings_filename(self, special=None):
-        special = " (" + special + ")" if special else ""
-        return "".join((self.package_name, special, ".sublime-settings"))
-
-    def get_hostplatform_setting(self, setting_name, default=None):
-        return self.hostplatform_settings.get(setting_name, default)
-
-    def get_host_setting(self, setting_name, default=None):
-        return self.host_settings.get(setting_name, default)
-
-    def get_platform_setting(self, setting_name, default=None):
-        return self.platform_settings.get(setting_name, default)
-
-    def get_user_setting(self, setting_name, default=None):
-        return self.user_settings.get(setting_name, default)
-
-    def get_settings_file_path(self, scope):
-        return "${packages}/%0s/%1s" % self.get_settings_pieces(scope)
-
-    def get_settings_pieces(self, scope):
-        if scope == SCOPE_HOST_OS:
-            return ("User", self.get_hostplatform_settings_filename())
-        if scope == SCOPE_HOST:
-            return ("User", self.get_host_settings_filename())
-        elif scope == SCOPE_USER:
-            return ("User", self.get_settings_filename())
-        elif scope == SCOPE_OS:
-            return ("User", self.get_platform_settings_filename())
-        elif scope == SCOPE_DEFAULT:
-            return (self.basepackage, self.get_settings_filename())
-        else:
-            raise Exception("Invalid scope specified: %s" % (scope))
+        for item in self.scoped_settings.values():
+            item.settings.clear_on_change(event_name)
 
     def open_settings(self, window, scope=None):
         def do_open_settings(selected_scope):
-            settings_file_path = self.get_settings_file_path(selected_scope)
+            default_path = _build_settings_file_path(
+                self.default_settings_dir, self.scoped_settings[SCOPE_DEFAULT].filename
+            )
+            user_path = _build_settings_file_path(
+                "User", self.scoped_settings[selected_scope].filename
+            )
 
-            self.log("Opening %s" % (settings_file_path,))
+            _logger.info("Opening settings files: ", default_path, user_path)
 
-            window.run_command("open_file", {"file": settings_file_path})
+            window.run_command(
+                "edit_settings",
+                {"base_file": default_path, "user_file": user_path, "default": "{}"},
+            )
 
         def on_ask_scope_done(selected_index):
             if selected_index < 0:
@@ -163,20 +97,49 @@ class _BetterSettings(object):
         if scope is None:
             ask_scope_and_open_settings()
         else:
+            _ensure_valid_scope(scope)
             do_open_settings(scope)
 
-    def log(self, *args):
-        if __DEBUG__:
-            print(
-                "[BetterSettings(%s:%s)][%s]"
-                % (
-                    self.basepackage,
-                    self.package_name,
-                    datetime.datetime.now().strftime("%H:%M:%S.%f"),
-                ),
-                *args
-            )
+
+class _ScopedSettings(object):
+    def __init__(self, filename, settings):
+        self.filename = filename
+        self.settings = settings
 
 
-def load_for(basepackage, package_name):
-    return _BetterSettings(basepackage, package_name)
+def load_for(default_settings_dir, package_name):
+    def get_settings_filename(special=None):
+        special = " (" + special + ")" if special else ""
+        return "".join((package_name, special, ".sublime-settings"))
+
+    def load_scoped_settings(filename):
+        return _ScopedSettings(filename, sublime.load_settings(filename))
+
+    scoped_settings = OrderedDict()
+    scoped_settings[SCOPE_HOST_OS] = load_scoped_settings(
+        get_settings_filename(
+            platform.uname()[1].lower() + " on " + sublime.platform().capitalize()
+        )
+    )
+    scoped_settings[SCOPE_HOST] = load_scoped_settings(
+        get_settings_filename(platform.uname()[1].lower())
+    )
+
+    scoped_settings[SCOPE_OS] = load_scoped_settings(
+        get_settings_filename(sublime.platform().capitalize())
+    )
+
+    scoped_settings[SCOPE_DEFAULT] = load_scoped_settings(get_settings_filename())
+
+    return _BetterSettings(default_settings_dir, scoped_settings)
+
+
+def _ensure_valid_scope(scope):
+    if scope not in _valid_scopes:
+        raise Exception(
+            "Invalid scope: %s (Valid scopes are %s)", (scope, _valid_scopes)
+        )
+
+
+def _build_settings_file_path(directory, filename):
+    return "${packages}/%0s/%1s" % (directory, filename)
